296af9364ace40b2b7cb4a8c660601828f16e3f7
dotnotation <dotinmotionproductions@gmail.com>
2022-02-22 15:25:07 -0500

    commit tree c9eb2bbd12a66016c1872296dd256e8b051033ec

--- src/containers/BandsContainer.js
+++ src/containers/BandsContainer.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import BandInput from '../components/BandInput'
+import Band from '../components/Band'
 
 class BandsContainer extends Component {
+  renderBands = () => this.props.bands.map((band, index) => <Band key={index} text={band} />)
+
   render() {
     return(
       <div>
-        BandsContainer
+        <BandInput addBand={this.props.addBand} />
+        <ol>
+          {this.renderBands()}
+        </ol>
       </div>
     )
   }
 }
 
-export default BandsContainer
+const mapStateToProps = (state) => {
+  return {
+    bands: state.bands
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addBand: form => dispatch({ type: "ADD_BAND", payload: form})
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BandsContainer)
